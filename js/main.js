cee09f77fecdca3df5e351749f92be0f3ad6ff28
jlstory <jlstory1@yahoo.com>
2023-10-24 15:24:06 -0500

    Continued Editing and Addition

--- js/main.js
+++ js/main.js
@@ -1,27 +1,12 @@
 //main js
 
-// Get the modal
-var modal = document.getElementById("myModal");
+$('#audio-control').click(function() {
+    if ($("#bauhausVideo").prop('muted')) {
+        $("#bauhausVideo").prop('muted', false);
+        $(this).text('turn sound off');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    } else {
+        $("#bauhausVideo").prop('muted', true);
+        $(this).text('turn sound on');
     }
-}
+});
